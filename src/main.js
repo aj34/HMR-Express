3c3cfe13fbde5b2b0253e37e49095c64e0f8c471
April Johnson <ajohnson@extensis.com>
2016-08-15 15:39:14 -0700

    bar chart with changing data

--- src/main.js
+++ src/main.js
@@ -2,56 +2,124 @@ import d3 from 'd3';
 import _ from 'lodash';
 import './assets/styles.css';
 
-// using lodash for random data
-const dataSet = _.map(_.range(25), () => {
-  return {
-    x: Math.random() * 100,
-    y: Math.random() * 100,
-    radius: Math.random() * 40
-  };
-});
+/// bar chart
 
-// all d3 code below
+const INDIVIDUAL_DATA = 'http://54.213.83.132/hackoregon/http/oregon_individual_contributors/10/';
+const COMMITTEE_DATA = 'http://54.213.83.132/hackoregon/http/oregon_committee_contributors/10/';
+const BUSINESS_DATA = 'http://54.213.83.132/hackoregon/http/oregon_business_contributors/10/';
+
+
 const margin = {
-  top: 0,
-  right: 0,
-  bottom: 20,
-  left: 50
+  top: 10,
+  right: 75,
+  bottom: 100,
+  left: 75
 };
-const width = 600 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
 
-const svg = d3.select('#content').append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-  .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+var height = 400; // set vars for height & width
+var width = 600;
 
-const yScale = d3.scale.linear()
-  .domain([0, d3.max(dataSet, data => {
-    return data.y;
-  })])
-  .range([height, 0]);
+var svg = d3.select('#barChart')
+  .append('svg')
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-const xScale = d3.scale.linear()
-  .domain([0, 100])
-  .range([0, width]);
+var yScale = d3.scale.linear()
+  .range([height, 0]) // set yScale linear
 
-svg.selectAll('circle')
-  .data(dataSet)
-  .enter()
-  .append('circle')
-  .attr('class', 'bubble')
-  .attr('cx', data => {
-    return xScale(data.x);
-  })
-  .attr('cy', data => {
-    return yScale(data.y);
-  })
-  .attr('r', data => {
-    return data.radius;
+var xScale = d3.scale.ordinal()
+  .rangeBands([0, width], 0.25, 0.25); // (width of data), padding between, padding outside
+
+var xAxis = d3.svg.axis()
+  .scale(xScale)
+  .orient("bottom")
+  .ticks(10);
+
+var yAxis = d3.svg.axis()
+  .scale(yScale)
+  .orient("left")
+  .ticks(10);
+
+const visualize = (url) => {
+  d3.json(url, function (json) {
+    let data = json;
+    let max = d3.max(data, d => d.sum);
+
+    yScale.domain([0, max + 100000]); // domain manually set a little higher than max value
+    xScale.domain(data.map(function (d) { return d.contributor_payee; }));
+
+    let bar = svg.selectAll('rect')
+      .data(data, function(d) { return d.contributor_payee; })
+
+      bar.enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', function (d) { return xScale(d.contributor_payee) })
+        .attr('y', function (d) { return yScale(d.sum) })
+        .attr('width', xScale.rangeBand()) // width determined by xScale.rangeBand
+        .style('height', function (d) { return height - yScale(d.sum) })
+
+      bar.exit().remove();
+
+      bar
+       .transition().duration(750)
+       .attr('x', function (d) { return xScale(d.contributor_payee) })
+       .attr('y', function (d) { return yScale(d.sum) })
+       .attr('width', xScale.rangeBand()) // width determined by xScale.rangeBand
+       .style('height', function (d) { return height - yScale(d.sum) })
+
+    svg.select(".y.axis").remove();
+    svg.select(".x.axis").remove();
+    svg.selectAll("text").remove();
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)");
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+  });
+}
+
+const visualizeWithChoice = (url) => {
+  if (url) {
+    visualize(url);
+  } else {
+    switch ($('select option:selected').val()) {
+      case 'individual':
+        visualize(INDIVIDUAL_DATA);
+        break;
+      case 'business':
+        return visualize(COMMITTEE_DATA);
+        break;
+      case 'committee':
+        return visualize(BUSINESS_DATA);
+        break;
+      default:
+    }
+  }
+}
+
+
+// functions for the page interactions
+$('#options')
+  .on('change', (event, index, value) => {
+    return visualizeWithChoice();
   });
 
+// initial  chart
+visualizeWithChoice(INDIVIDUAL_DATA);
 
 /*
 * ignore this code below - it's for webpack to know that this
